825176e7b3e6b27185b8b7a0ce9b37fabf7b70a2
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-02-19 10:53:00 +0200

    Calculating the total expenses and adding reminder for salary - total_exp

--- salary.py
+++ salary.py
@@ -2,7 +2,7 @@
 salary = float(input("Enter your salary: "))
 month = input("Enter the name of the month: ")
 
-# Ask for percentages of salary allocated to savings rent and electricity
+# Asking for percentages of salary allocated to savings rent and electricity
 savings_per = float(input("Enter the percentage of savings: "))
 rent_per = float(input("Enter the percentage of rent: "))
 electricity_per = float(input("Enter the Percentage of electricity: "))
@@ -11,3 +11,7 @@ electricity_per = float(input("Enter the Percentage of electricity: "))
 savings = (savings_per / 100) * salary
 rent = (rent_per / 100) * salary
 electricity = (electricity_per / 100) * salary
+
+# Calculating the total expenses and reminder
+total_exp = savings + rent + electricity
+reminder = salary - total_exp
