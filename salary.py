0e31a75feff51ac4feb396732792dc9f96a732ab
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-02-19 10:47:26 +0200

    Calculating the amount for savings, rent, elect after searching it up on google :)

--- salary.py
+++ salary.py
@@ -6,3 +6,8 @@ month = input("Enter the name of the month: ")
 savings_per = float(input("Enter the percentage of savings: "))
 rent_per = float(input("Enter the percentage of rent: "))
 electricity_per = float(input("Enter the Percentage of electricity: "))
+
+# Calculating the amount for savings, rent, electricity
+savings = (savings_per / 100) * salary
+rent = (rent_per / 100) * salary
+electricity = (electricity_per / 100) * salary
