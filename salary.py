5127fb0107aa503abb82c844aa9117b7cf55a259
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-02-19 10:41:50 +0200

    Asking user (Nabiha) to input the percenategs of saving, rent, electricity

--- salary.py
+++ salary.py
@@ -1,3 +1,8 @@
 # Asking for salary and moth
 salary = float(input("Enter your salary: "))
 month = input("Enter the name of the month: ")
+
+# Ask for percentages of salary allocated to savings rent and electricity
+savings_per = float(input("Enter the percentage of savings: "))
+rent_per = float(input("Enter the percentage of rent: "))
+electricity_per = float(input("Enter the Percentage of electricity: "))
